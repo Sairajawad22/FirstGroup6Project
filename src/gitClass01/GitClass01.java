fec93082d8fc79eda36ae563402a873bef3353fa
Sairajawad22 <sairajawad32@gmail.com>
2022-09-15 12:04:00 -0400

    first commit

---
+++ src/gitClass01/GitClass01.java
@@ -0,0 +1,11 @@
+package gitClass01;
+
+public class GitClass01 {
+
+	public static void main(String[] args) {
+		// code to launch the nuclear weapon
+		System.out.println("Launch my favorite TV show ");
+
+	}
+
+}
